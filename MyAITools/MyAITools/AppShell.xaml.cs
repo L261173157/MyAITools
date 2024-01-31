32e74cb934697de4fb9acb93f3f7f9466bea951b
LinX <qiuxiaoku@gmail.com>
2024-01-31 15:11:10 +0800

    initial

---
+++ MyAITools/MyAITools/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MyAITools;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MyAITools/MyAITools/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MyAITools;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
