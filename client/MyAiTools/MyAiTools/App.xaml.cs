6ed1f1bfcb457d3c762c1619438a45a67076937a
LinX <qiuxiaoku@gmail.com>
2024-02-19 16:59:58 +0800

    xiuzheng

---
+++ client/MyAiTools/MyAiTools/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MyAiTools;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ client/MyAiTools/MyAiTools/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MyAiTools;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
